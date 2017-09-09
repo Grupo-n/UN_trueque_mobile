36f06990906a087e4a9f70aa9b58d4d3348d5c15
joagranadosme <joagranadosme@unal.edu.co>
2017-09-09 12:43:23 -0500

    First commit.

--- app/src/main/java/co/edu/unal/un_trueque/MainActivity.java
+++ app/src/main/java/co/edu/unal/un_trueque/MainActivity.java
@@ -3,6 +3,8 @@ package co.edu.unal.un_trueque;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void searchClick(View view){
+        Toast.makeText(view.getContext(), "Search", Toast.LENGTH_LONG).show();
+    }
+
+    public void menuClick(View view){
+        Toast.makeText(view.getContext(), "Menu", Toast.LENGTH_LONG).show();
     }
 
 }
